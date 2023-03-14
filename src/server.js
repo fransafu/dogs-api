f91ed31fda33086dfe0b72e7978a711df7513c1e
Francisco Sanchez <fransafu@gmail.com>
2023-03-14 20:45:56 -0300

    feat(server): add express app listen on server

--- src/server.js
+++ src/server.js
@@ -1,11 +1,16 @@
 require('dotenv').config()
 
+const app = require('./app');
 const { sequelize } = require('./models');
+
+const PORT = process.env.PORT || 3000;
 
 sequelize
     .sync()
     .then((resp) => {
-        console.log(resp);
+        app.listen(PORT, () => {
+            console.log('Listen on port ', PORT);
+        })
     })
     .catch((err) => {
         console.log(err);
