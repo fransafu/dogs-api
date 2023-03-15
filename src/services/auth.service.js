ced644a2dbb1680c27d0a41c62f7e889da8a6fed
Francisco Sanchez <fransafu@gmail.com>
2023-03-14 21:42:25 -0300

    feat: add auth service

---
+++ src/services/auth.service.js
@@ -0,0 +1,38 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const { models: { User } } = require('../models');
+const { compareHashPassword } = require('../utils/hash');
+
+const userService = require('./user.service');
+
+const login = async (username, password, salt = 0) => {
+    const user = await User.findOne({
+        where: {
+            username,
+        }
+    });
+
+    if (!user) {
+        return null;
+    }
+
+    if (!compareHashPassword(password, user.password)) {
+        return null;
+    }
+
+    return jwt.sign({
+        username: user.get().username,
+        email: user.get().email,
+    }, process.env.SECRET_JWT);
+};
+
+const register = async (user) => {
+    const newUser = await userService.create(user);
+    return newUser;
+}
+
+module.exports = {
+    login,
+    register,
+};
