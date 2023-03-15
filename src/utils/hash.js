f8f14a175754d106013b413db13adbf7307db6a0
Francisco Sanchez <fransafu@gmail.com>
2023-03-14 21:36:45 -0300

    feat: add utils module with hash functions

---
+++ src/utils/hash.js
@@ -0,0 +1,12 @@
+const generateHashFromPassword = async (password, hash = 10) => {
+    return await bcrypt.hash(password, salt);
+}
+
+const compareHashPassword = async (passwordTextPlain, hash) => {
+    return await bcrypt.compare(passwordTextPlain, hash);
+}
+
+module.exports = {
+    generateHashFromPassword,
+    compareHashPassword,
+};
