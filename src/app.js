ac0f1ea9aed8e6fa539a4837d415b87cb5c0cc30
Francisco Sanchez <fransafu@gmail.com>
2023-03-15 00:23:49 -0300

    feat: enable cors, morgan and helmet ad middlewares (sec, permission and logs)

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const helmet = require('helmet');
 
 const router = require('./routes');
 
 const app = express();
 
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(morgan('combined'));
 
 app.use('/', router);
 
