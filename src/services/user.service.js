6082016fb2443b95125011f356704afc1018cf75
Francisco Sanchez <fransafu@gmail.com>
2023-03-14 21:42:02 -0300

    refactor: implement utils hash and clear code

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,14 +1,23 @@
-const { models: { User } } = require('../models');
+const bcrypt = require('bcrypt');
+
+const { models: { User, Rol } } = require('../models');
+const { generateHashFromPassword } = require('../utils/hash');
 
 const list = async () => {
-    const users = await User.findAll();
+    const users = await User.findAll({
+        attributes: ['username', 'email'],
+        include: {
+            attributes: ['type'],
+            model: Rol, as: 'rol'
+        }
+    });
     return users.map((user) => user.get());
 };
 
-const create = async (user) => {
-    const newUser = await User.create(user)
-    console.log(newUser);
-    return newUser;
+const create = async (user, salt = 10) => {
+    return await User.create({
+        ...user,
+        password: generateHashFromPassword(user.password) });
 }
 
 module.exports = {
