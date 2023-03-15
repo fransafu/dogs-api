d93bff96d2cef0e43c0675742c37e866e1c7c5d6
Francisco Sanchez <fransafu@gmail.com>
2023-03-15 00:43:49 -0300

    feat: add health-check endpoint

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 
+const healthCheckRouter = require('./health-check.route');
 const userRouter = require('./user.route');
 const authRouter = require('./auth.route');
 const breedRouter = require('./breed.route');
 
 const router = express.Router();
 
+router.use('/', healthCheckRouter);
 router.use('/users', userRouter);
 router.use('/auth', authRouter);
 router.use('/breeds', breedRouter);
